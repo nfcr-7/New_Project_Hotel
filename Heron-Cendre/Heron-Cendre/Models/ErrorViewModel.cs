5e7c49bfe5c5f1235da5db6e45698157b1c18173
Rabier <=>
2023-02-28 14:14:09 +0100

    New Project

---
+++ Heron-Cendre/Heron-Cendre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Heron_Cendre.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
