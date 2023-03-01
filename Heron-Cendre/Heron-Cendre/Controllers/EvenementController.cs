97d119b5c521e0bebb85c4dfeae8c603b00f6cc9
Nikiledji Aurore <auroranikiledji@gmail.com>
2023-03-01 16:00:05 +0100

    Creation du controller du client

---
+++ Heron-Cendre/Heron-Cendre/Controllers/EvenementController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Heron_Cendre.Controllers
+{
+    public class EvenementController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Heron-Cendre/Heron-Cendre/Data/Services/Clients/ClientServices.cs
@@ -0,0 +1,36 @@
+﻿namespace Heron_Cendre.Data.Services.Clients
+{
+    public class ClientServices : InterfClentService
+    {
+
+        private readonly AppDbContext _context;
+
+        public ClientServices(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(Client client)
+        {
+            await _context.Client.AddAsync(client);
+            await _context.SaveChangesAsync();
+                
+        }
+
+        public async Task<IEnumerable<Client>> GetAll()
+        {
+            var result = await _context.clients.ToListAsync();
+            return result;
+        }
+
+        public Client GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Client Update(int id, Actor newClient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
